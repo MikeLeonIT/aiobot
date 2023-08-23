0aa77f6ead4221ccf2385510e99935ff54f36a82
Mikhail Leonov <MikeLeonIt@yandex.ru>
2023-08-23 16:23:17 +0300

    23/08

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,16 @@
+
+l
+<venv/Lib/site-packages/aiogram/dispatcher/filters/filters.py,8\d\8de631612d0d270d8470347fc02dafcc2065e3bb
+]
+-.idea/sonarlint/securityhotspotstore/index.pb,8\8\88a9255124c95bdc913197c120a8d560edc59c8e
+S
+#.idea/sonarlint/issuestore/index.pb,9\f\9fe84ebb15faf917b7def6236dba604453cc61e0
+^
+..idea/inspectionProfiles/profiles_settings.xml,1\e\1e9075f5bf079c01ef2c910709e91a497d262080
+\
+,.idea/inspectionProfiles/Project_Default.xml,4\9\496a238a6afa168dbaf6efd37bb459331589579c
+:
+
+readme.txt,4\5\451685e9efac4a6dc1fee73ec53ffb6b2c4c38b5
+<
+asinc_bot.py,d\b\dba052573942e110464ec664982a768b796bddd1
--- asinc_bot.py
+++ asinc_bot.py
@@ -1,8 +1,36 @@
+import asyncio
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.dispatcher.filters import Text
 
-API_TOKEN = '6392530153:AAFLBiS0l06BQdxyVNJ9sGSlkFPDjaImxWY' # token бота телега
+API_TOKEN = '6392530153:AAFLBiS0l06BQdxyVNJ9sGSlkFPDjaImxWY'  # token бота телега
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
 
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    await message.reply('Привет! Вложенные функции:\n'
+                        '/document')
 
+
+@dp.message_handler(commands="document")
+async def cmd_start(message: types.Message):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    buttons = ["...Да...", "...Нет..."]
+    keyboard.add(*buttons)
+    await message.answer("Загрузить файл?", reply_markup=keyboard)
+    @dp.message_handler(Text(equals="...Да..."))
+    async def with_puree(message: types.Message):
+        await message.reply("Отличный выбор!")
+        async def save_file()
+
+    @dp.message_handler(lambda message: message.text == "...Нет...")
+    async def without_puree(message: types.Message):
+        await message.reply("Ну и ладно!")
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
