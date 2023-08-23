54d201cf24a3d34e95a5dbbea738ca48ff0dc0bb
Mikhail Leonov <MikeLeonIt@yandex.ru>
2023-08-23 13:47:58 +0300

    new

---
+++ asinc_bot.py
@@ -0,0 +1,8 @@
+from aiogram import Bot, Dispatcher, executor, types
+
+API_TOKEN = '6392530153:AAFLBiS0l06BQdxyVNJ9sGSlkFPDjaImxWY' # token бота телега
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+
